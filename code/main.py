acaad1af3b17c2f178c25a14c3cd5d512d1665cb
Minhyeok-Oh <alsgur2848@daum.net>
2021-02-10 15:25:05 +0900

    2021-02-10

--- code/main.py
+++ code/main.py
@@ -1,12 +1,42 @@
-#!/usr/bin/env python
+import cv2
+import imagehash
+from PIL import Image
 
-# Code from the wikipedia page for Viterbi algorithm done or modified by Zhubarb
-# More implementations of Viterbi algorithm can be found at http://stackoverflow.com/questions/9729968/python-implementation-of-viterbi-algorithm
-# Example of implementation of the viterbi algorithm for a  primitive clinic in a village.
-# People in the village have a very nice property that they are either healthy or have a fever.
-# They can only tell if they have a fever by asking a doctor in the clinic.
-# The wise doctor makes a diagnosis of fever by asking patients how they feel.
-# Villagers only answer that they feel normal, dizzy, or cold.
+def get_hash(vidcap, sec, count, video_filename, hashes):
+    vidcap.set(cv2.CAP_PROP_POS_MSEC, sec*1000)
+    hasFrames, image = vidcap.read()
+
+    if hasFrames:
+        image = cv2.resize(image, (256, 256))
+        hash_img = imagehash.average_hash(Image.fromarray(image))
+        hashes.append({"hash": hash_img, "count": count, "sec": sec})
+    return hasFrames
+
+def video_to_hashes(video_filename, hashes):
+
+    vidcap = cv2.VideoCapture(video_filename)
+    sec = 0
+    frameRate = 0.3 #//it will capture image in each 0.5 second
+    count = 1
+    success = get_hash(vidcap, sec, count, video_filename, hashes)
+    while success:
+        count = count + 1
+        sec = sec + frameRate
+        sec = round(sec, 2)
+        if sec >= 480:
+            break
+        success = get_hash(vidcap, sec, count, video_filename, hashes)
+    vidcap.release()
+
+
+hashes_A = []
+hashes_B = []
+
+video_to_hashes('C:/Users/alsgu/Downloads/편의점 샛별이.E01.200620.1080p.WEB-DL.x264.AAC-Deresisi.mp4', hashes_A)
+video_to_hashes('C:/Users/alsgu/Downloads/편의점 샛별이.E02.200620.1080p.WEB-DL.x264.AAC-Deresisi.mp4', hashes_B)
+
+
+
 states = ('intro', 'none')
 observations = ('(0,0)', '(0,0)', '(1,0)', '(1,1)', '(1,0)', '(0,0)', '(0,0)', '(1,0)', '(1,1)', '(1,1)')
 start_probability = {'intro': 0.43, 'none': 0.57}
@@ -14,7 +44,7 @@ transition_probability = {
     'intro': {'intro': 0.63, 'none': 0.37},
     'none': {'intro': 0.44, 'none': 0.56}
 }
-#
+
 emission_probability = {
     'intro': {'(0,0)': 0.28, '(1,0)': 0.32, '(1,1)': 0.4},
     'none': {'(0,0)': 0.43, '(1,0)': 0.24, '(1,1)': 0.23}
@@ -78,3 +108,15 @@ def example():
 
 
 print(example())
+
+print(hashes_A, hashes_B)
+
+result = []
+
+for hash_A in hashes_A:
+    for hash_B in hashes_B:
+        diff = hash_A["hash"] - hash_B["hash"]
+        if diff < 5:
+            result.append({"count": hash_A["count"], "sec": hash_A["sec"]})
+            break
+print(result)
